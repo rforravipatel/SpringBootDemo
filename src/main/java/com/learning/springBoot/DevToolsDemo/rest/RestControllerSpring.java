7e9697b3435c1a75a0024b5e2cf478d43243f169
Ravi <tempravi789@gmail.com>
2019-04-23 19:22:48 -0400

    add properties

--- src/main/java/com/learning/springBoot/DevToolsDemo/rest/RestControllerSpring.java
+++ src/main/java/com/learning/springBoot/DevToolsDemo/rest/RestControllerSpring.java
@@ -2,11 +2,23 @@ package com.learning.springBoot.DevToolsDemo.rest;
 
 import java.time.LocalDateTime;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RestControllerSpring {
+
+	@Value("${coach.name}")
+	private String coachName;
+
+	@Value("${team.name}")
+	private String teamName;
+
+	@GetMapping("/teaminfo")
+	public String getTeamInfo() {
+		return "Coach: " + coachName + ", Team name: " + teamName;
+	}
 
 	@GetMapping("/")
 	public String sayHello() {
